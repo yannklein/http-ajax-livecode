38b5b586982234e176672856d85c8b06b54e0702
Yann Klein <yann.klein@me.com>
2020-12-16 02:28:26 +0000

    homepage deployed

--- dist/main.js
+++ dist/main.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/clearbit.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -90,10 +90,68 @@
 /*!*************************!*\
   !*** ./lib/clearbit.js ***!
   \*************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! exports provided: fetchClearbit */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fetchClearbit", function() { return fetchClearbit; });
 const authorization = "Bearer sk_f9deb0090ac7688f94eb11ce09fd6a6e";
+
+const email = document.querySelector("#userEmail");
+const name = document.querySelector("#userName");
+const bio = document.querySelector("#userBio");
+const location = document.querySelector("#userLocation");
+
+const displayStalkingData = (personData) => {
+  email.innerText = personData.email;
+  name.innerText = personData.name.fullName;
+  bio.innerText = personData.bio;
+  location.innerText = personData.location;
+}
+const fetchClearbit = (email) => {
+  const url = `https://person.clearbit.com/v2/combined/find?email=${email}`;
+  // console.log(event);
+  // Step 2.5: Fetch an API: and get stalking data
+  fetch(url, {
+    headers: {
+      Authorization: authorization
+    }
+  })
+    .then(response => response.json())
+    .then((data) => {
+      // console.log(data);
+      // Step 3: Change the DOM: display the stalking data in the tds
+      displayStalkingData(data.person);
+    });
+}
+
+
+
+
+/***/ }),
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _clearbit__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./clearbit */ "./lib/clearbit.js");
+
+
+// Step 1: Select elements: input, button, 4 tds
+const input = document.querySelector("#clearbitEmail");
+const button = document.querySelector("#clearbitSubmit");
+
+// Step 2: Listen to an event: listen a a "submit" on the button (don't forget .preventDefault())
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  Object(_clearbit__WEBPACK_IMPORTED_MODULE_0__["fetchClearbit"])(input.value);
+});
 
 
 /***/ })
